2d61a595234899094bcc8aedb05c14ef01954a9f
Pascal COMBIER <103032158+pascalcombier@users.noreply.github.com>
2026-03-01 17:41:43 +0800

    Improve format and packages

--- apm/packages/lua/5.5/index.lua
+++ apm/packages/lua/5.5/index.lua
@@ -1,37 +1,235 @@
-{
-  {
-    filename = "30log-0.8.0.zip",
-    dependencies = {
-      "lua >= 5.1",
-    },
+fileformat = "apm-index-v1",
+index = {
+  ["3rd-party-oauth-1.0.zip"] = {
+    ["3rd-party-oauth-1.0"] = {
+      dependencies = {
+        "lua >= 5.1",
+        "luacrypto >= 0.3"
+      }
+    }
   },
-  {
-    filename = "ac-clientoutput-1.1.1.zip",
-    dependencies = {
-      "lua >= 5.1",
-    },
+  ["30log-0.2.zip"] = {
+    ["30log-0.2"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
   },
-  {
-    filename = "abacatepaysdk-1.0.zip",
-    dependencies = {
-      "socket >= 5.1",
-      "ltn12 >= 5.1",
-    },
+  ["30log-0.3.zip"] = {
+    ["30log-0.3"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
   },
-  {
-    filename = "abstk-release-1.zip",
-    dependencies = {
-      "lgi >= 0.9.1",
-      "lcurses >= 9.0",
-    },
+  ["30log-0.4.1.zip"] = {
+    ["30log-0.4.1"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
   },
-  {
-    filename = "access-token-introspection-1.0.0.zip",
+  ["30log-0.4.zip"] = {
+    ["30log-0.4"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
   },
-  {
-    filename = "uint-0.186.zip",
-    dependencies = {
-      "lua >= 5.1",
-    },
+  ["30log-0.5.0.zip"] = {
+    ["30log-0.5.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
   },
+  ["30log-0.6.0.zip"] = {
+    ["30log-0.6.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["30log-0.7.0.zip"] = {
+    ["30log-0.7.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["30log-0.8.0.zip"] = {
+    ["30log-0.8.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["dkjson-2.8.zip"] = {
+    ["dkjson-2.8"] = {
+      dependencies = {
+        "lua >= 5.1, < 5.6"
+      }
+    }
+  },
+  ["dromozoa-json-1.0.zip"] = {
+    ["dromozoa-json-1.0"] = {
+      dependencies = {
+        "dromozoa-utf8 >= 1.2"
+      }
+    }
+  },
+  ["dromozoa-json-1.1.zip"] = {
+    ["dromozoa-json-1.1"] = {
+      dependencies = {
+        "dromozoa-utf8 >= 1.2"
+      }
+    }
+  },
+  ["dromozoa-json-1.2.zip"] = {
+    ["dromozoa-json-1.2"] = {
+      dependencies = {
+        "dromozoa-utf8 >= 1.2"
+      }
+    }
+  },
+  ["dromozoa-json-1.3.zip"] = {
+    ["dromozoa-json-1.3"] = {
+      dependencies = {
+        "dromozoa-utf8 >= 1.3"
+      }
+    }
+  },
+  ["hjson-lua-0.1.2.zip"] = {
+    ["hjson-lua-0.1.2"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["hjson-lua-0.1.3.zip"] = {
+    ["hjson-lua-0.1.3"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["hjson-lua-0.1.4.zip"] = {
+    ["hjson-lua-0.1.4"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["hjson-lua-0.1.5.zip"] = {
+    ["hjson-lua-0.1.5"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["hjson-lua-0.1.6.zip"] = {
+    ["hjson-lua-0.1.6"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["hjson-lua-0.2.0.zip"] = {
+    ["hjson-lua-0.2.0"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["hjson-lua-0.3.0.zip"] = {
+    ["hjson-lua-0.3.0"] = {
+      dependencies = {
+        "lua >= 5.3"
+      }
+    }
+  },
+  ["json-lua-0.1.zip"] = {
+    ["json-lua-0.1"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["json-rock-2.0.zip"] = {
+    ["json-rock-2.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lua-jsonpatch-0.zip"] = {
+    ["lua-jsonpatch-0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lua-ubjson-0.1.0.zip"] = {
+    ["lua-ubjson-0.1.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lunajson-1.0.zip"] = {
+    ["lunajson-1.0"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lunajson-1.1.zip"] = {
+    ["lunajson-1.1"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lunajson-1.2.1.zip"] = {
+    ["lunajson-1.2.1"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lunajson-1.2.2.zip"] = {
+    ["lunajson-1.2.2"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lunajson-1.2.3.zip"] = {
+    ["lunajson-1.2.3"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["lunajson-1.2.zip"] = {
+    ["lunajson-1.2"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["moonjson-0.1.2.zip"] = {
+    ["moonjson-0.1.2"] = {
+      dependencies = {
+        "lua >= 5.1"
+      }
+    }
+  },
+  ["penlight-1.10.0.zip"] = {
+    ["penlight-1.10.0"] = {
+      dependencies = {
+        "luafilesystem"
+      }
+    }
+  }
 }
